decdcc9c94a5f85ad88dfcfd6bb4c5b984d392df
Elijah M. Immer <me@eimmer.me>
2023-11-23 02:49:29 +0000

    Added some small server config

--- hosts/server/configuration.nix
+++ hosts/server/configuration.nix
@@ -20,11 +20,40 @@
     misc.common
   ];
 
-  services.openssh = {
-    enable = true;
-    startWhenNeeded = true;
-    # TODO: Add age nix or another secrets method and get the authorized keys here!
-    banner = ''===========================================\nHERE BE DRAGONS; BEWARE OF WATERS AHEAD\n===========================================\n'';
+  services = {
+    openssh = {
+      enable = true;
+      startWhenNeeded = true;
+      # TODO: Add age nix or another secrets method and get the authorized keys here!
+      banner = ''
+        ===========================================
+          HERE BE DRAGONS; BEWARE OF WATERS AHEAD
+        ===========================================
+      '';
+    };
+    btrfs.autoScrub.enable = true;
+  };
+
+  security = {
+    tpm2.enable = true;
+    protectKernelImage = true;
+    polkit.enable = true;
+  };
+
+  hardware = {
+    enableAllFirmware = true;
+    opengl = {
+      enable = true;
+      driSupport32Bit = true;
+      extraPackages = with pkgs; [
+        vulkan-validation-layers
+        vulkan-extension-layer
+        vulkan-loader
+        vulkan-tools
+      ];
+    };
+
+    cpu.intel.updateMicrocode = true;
   };
 
   boot = {

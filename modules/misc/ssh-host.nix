03be503e2bb83e46dd705ff46e87c9e9b3a89312
Elijah M. Immer <me@eimmer.me>
2024-01-29 18:42:01 -0800

    edit ssh-host

--- modules/misc/ssh-host.nix
+++ modules/misc/ssh-host.nix
@@ -8,7 +8,4 @@
 
     openFirewall = lib.mkForce true;
   };
-  users.users.eimmer.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKtGu3F4RIogpoh5cai+8+kF6Ec8WJoM+1UfpmqPLB7v eimmer@lv14"
-  ];
 }

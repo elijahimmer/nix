c690de449c0246d12628848a72a2811cd552747d
Elijah M. Immer <me@eimmer.me>
2024-03-29 23:08:03 -0700

    fix steam to use the correct resolution

--- modules/eimmer/games.nix
+++ modules/eimmer/games.nix
@@ -8,7 +8,7 @@
       pkgs.writeShellScriptBin "steam-bigpicture" ''
         ${lib.getExe' pkgs.gamemode "gamemoderun"} ${lib.getExe pkgs.gamescope} \
           -fe --force-grab-cursor --sharpness 0 \
-          -H 1080 -W 1920 -S integer -- ${lib.getExe pkgs.steam}
+          -H 1440 -W 3440 -S integer -- ${lib.getExe pkgs.steam} -tenfoot -pipewire-dmabuf
       ''
     )
   ];

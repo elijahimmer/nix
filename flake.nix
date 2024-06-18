20faced2d4b2b524257158d0392c379fd51d2adc
Elijah M. Immer <me@eimmer.me>
2024-06-17 20:56:06 -0700

    update inputs

--- flake.nix
+++ flake.nix
@@ -113,7 +113,7 @@
 
             inputs.nixos-hardware.nixosModules.common-cpu-amd
             inputs.nixos-hardware.nixosModules.common-cpu-amd-pstate
-            inputs.nixos-hardware.nixosModules.common-gpu-amd
+            #inputs.nixos-hardware.nixosModules.common-gpu-amd
             inputs.nixos-hardware.nixosModules.common-pc
             inputs.nixos-hardware.nixosModules.common-pc-ssd
           ]

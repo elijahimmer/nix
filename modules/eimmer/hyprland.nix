e600a28f124f832e87e07cc551547bf0dff3d5d4
Elijah M. Immer <me@eimmer.me>
2024-06-26 20:35:59 -0700

    raise lock timeout time to 10 minutes

--- modules/eimmer/hyprland.nix
+++ modules/eimmer/hyprland.nix
@@ -36,8 +36,8 @@
           after_sleep_cmd = "hyprctl dispatch dpms on";
         };
         listener = [
-          { timeout = 300;  on-timeout = "loginctl lock-session"; }
-          { timeout = 330;  on-timeout = "hyprctl dispatch dpms off"; on-resume = "hyprctl dispatch dpms on"; }
+          { timeout = 600;  on-timeout = "loginctl lock-session"; }
+          { timeout = 630;  on-timeout = "hyprctl dispatch dpms off"; on-resume = "hyprctl dispatch dpms on"; }
           { timeout = 1800; on-timeout = "systemctl suspend"; }
         ];
       };

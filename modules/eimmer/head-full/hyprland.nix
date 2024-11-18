3bc824653eeefc9fdba339952492a5780406940a
Elijah M. Immer <me@eimmer.me>
2024-11-17 16:59:59 -0800

    Update hyprland config for new update

--- modules/eimmer/head-full/hyprland.nix
+++ modules/eimmer/head-full/hyprland.nix
@@ -84,7 +84,6 @@ in {
           ];
           master = {
             mfact = 0.5;
-            no_gaps_when_only = true;
             orientation = "right";
           };
           general = {
@@ -128,6 +127,18 @@ in {
             "pin,   title:^(Picture-in-Picture)$"
             "move onscreen 100% 0, title:^(Picture-in-Picture)$"
             "suppressevent fullscreen, ^(Signal)$"
+          ];
+
+          workspace = [
+            "w[tv1], gapsout:0, gapsin:0"
+            "f[1], gapsout:0, gapsin:0"
+          ];
+
+          windowrulev2 = [
+            "bordersize 0, floating:0, onworkspace:w[tv1]"
+            "rounding 0, floating:0, onworkspace:w[tv1]"
+            "bordersize 0, floating:0, onworkspace:f[1]"
+            "rounding 0, floating:0, onworkspace:f[1]"
           ];
 
           bindm = [
